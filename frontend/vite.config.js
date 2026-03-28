0fd9e5d56fe8f56ea38a03a3188645f640a20e79
Milind Kulkarni <milindmk2001@gmail.com>
2026-03-28 19:06:12 +1000

    Merge remote and local Ascenda histories1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // If using the Vite plugin
+import tailwindcss from '@tailwindcss/vite' // Add this import
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(), // Add this to the plugins array
   ],
 })
